6a0fae831a82aadf2d3fadc069bab79e33814533
Vegard Falldalen <vegfall@gmail.com>
2023-09-25 16:49:59 +0200

    Added some logging and changing get request to send in json

--- src/main/java/httppackage/ApiServlet.java
+++ src/main/java/httppackage/ApiServlet.java
@@ -10,14 +10,18 @@ import java.io.IOException;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApiServlet extends HttpServlet {
+    private final static Logger logger = LoggerFactory.getLogger(ApiServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         StringBuilder response = new StringBuilder();
 
         if (AnimalRepository.getAnimals().isEmpty()) {
-            response.equals("No animals here :(");
+            response.append("No animals here :(");
         } else {
             AnimalRepository.getAnimals().forEach(animal -> {
                     response.append(String.format("%s named %s\r\n", animal.getType(), animal.getName()));
@@ -25,7 +29,9 @@ public class ApiServlet extends HttpServlet {
             );
         }
 
-        resp.getWriter().write(response.toString());
+        logger.info(response.toString());
+
+        resp.getWriter().write(new ObjectMapper().writeValueAsString(response.toString()));
     }
 
     @Override
